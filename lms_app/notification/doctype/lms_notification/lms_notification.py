8684a362d959a0e4c1c5b513c5eb54d51a40883f
urtaa <kyok309xx@gmail.com>
2025-11-20 15:54:22 +0800

    doctype fields added

---
+++ lms_app/notification/doctype/lms_notification/lms_notification.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, urtaa and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class LMS_notification(Document):
+	pass
---
+++ lms_app/quiz_system/doctype/quiz_question_answer/quiz_question_answer.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, urtaa and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Quiz_question_answer(Document):
+	pass
